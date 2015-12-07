677476e93f77340727097d6218ce7cdc25d48cba
m80126colin <m80126colin@gmail.com>
2015-12-07 22:17:25 +0800

    update json

--- translate/generator.js
+++ translate/generator.js
@@ -8,7 +8,7 @@ glob('../_posts/*.md', {}, function (err, files) {
   files.map(function (path_to_file) {
     var file = path.parse(path_to_file);
     var name = file.name.match(/^p?(\d+)$/)[1];
-    res[name] = file.name + '.html';
+    res[name] = file.name + '/';
   })
   fs.writeFile('translate.json', JSON.stringify(res));
 })
